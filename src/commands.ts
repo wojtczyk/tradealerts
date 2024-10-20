be045702d29ab1b36d461d5d56232879d233f8af
Martin Wojtczyk <martin@osdyne.com>
2024-10-19 18:26:31 -0700

    commands

---
+++ src/commands.ts
@@ -0,0 +1,14 @@
+export const commands = [
+    {
+        name: "General Commands",
+        triggers: ["/help"],
+        description: "Command for managing default behaviours.",
+        commands: [
+            {
+                command: "/help",
+                handler: undefined,
+                description: "Get help with the app.",
+            },
+        ],
+    },
+];
